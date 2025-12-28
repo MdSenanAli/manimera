be35205bf52b9fdb22b1ea4e0eac67380bc644dd
MohammadSenanAli <mdsenan@gmail.com>
2025-12-28 17:10:37 +0530

    feat: add Bohr's atom model as a Manim component with a demonstration example.

--- examples/components/demo_bohr_atom.py
+++ examples/components/demo_bohr_atom.py
@@ -3,8 +3,9 @@ from manimera import *
 
 class DemoBohrAtom(ManimeraScene):
     def create(self):
-        atom = BohrAtom(AtomType.Hydrogen)
-        self.add(atom)
+        atom = BohrAtom(AtomType.Oganesson, atom_scale=5)
+        name = atom.get_name().next_to(atom, direction=DOWN, buff=0.4)
+        self.add(atom, name)
 
 
 if __name__ == "__main__":
--- src/manimera/components/bohr_atom.py
+++ src/manimera/components/bohr_atom.py
@@ -39,7 +39,7 @@ class BohrAtom(VGroup):
 
     # INITIALIZATION ===================================================================================================
 
-    def __init__(self, atom_type: AtomType, atom_scale: int = 3, **kwargs) -> None:
+    def __init__(self, atom_type: AtomType, atom_scale: Optional[int] = None, **kwargs) -> None:
         """
         Initialize the Bohr atom.
 
@@ -55,11 +55,15 @@ class BohrAtom(VGroup):
         """
         super().__init__(**kwargs)
 
-        #
+        # Instance variables
         self.atom_type = atom_type
         self.mass_number = atom_type.value[1]
         self.atomic_number = atom_type.value[0]
         self.atom_scale = atom_scale
+
+        # Shell offset
+        self.shell_offset = 1
+        self.shell_stroke_width = 2
 
         # Create Bohr Atom
         self._create_bohr_atom()
@@ -83,16 +87,21 @@ class BohrAtom(VGroup):
             None
         """
         # Get subatomic particle counts
-        num_protons, num_electrons, num_neutrons = self._get_subatomic_particle_counts()
+        num_electrons = self._get_electrons()
+        num_neutrons = self._get_neutrons()
+        num_protons = self._get_protons()
 
-        # Create nucleus
+        # Create and add nucleus
         nucleus = self._create_nucleus()
-
-        # Add nucleus to the atom
         self.add(nucleus)
 
+        # Create and add shells
+        electrons, shells = self._create_electrons()
+        self.add(shells, electrons)
+
         # Scale the atom to fit the screen
-        # self.scale_to_fit_width(self.atom_scale)
+        if self.atom_scale is not None:
+            self.scale_to_fit_width(self.atom_scale)
 
         # Return
         return
@@ -113,34 +122,114 @@ class BohrAtom(VGroup):
             None
         """
         # Create nucleus
-        nucleus = Dot(radius=0.2, color=YELLOW)
+        nucleus = Dot(radius=0.2, color=YELLOW, stroke_width=0)
 
         # Return
         return nucleus
 
-    # HELPER METHODS ===================================================================================================
-
-    def _get_subatomic_particle_counts(self) -> Tuple[int, int, int]:
+    def _create_electrons(self) -> VGroup:
         """
-        Calculates the count of sub-atomic particles.
+        Create the shells of the atom.
 
         Args:
             None
 
         Returns:
-            A tuple containing the number of protons, electrons, and neutrons.
+            A VGroup object representing the shells of the atom.
 
         Raises:
             None
         """
+        # Create shells
+        shells = VGroup()
+        electrons = VGroup()
 
-        # Calculate subatomic particle counts
-        num_protons = self.atomic_number
-        num_electrons = self.atomic_number
-        num_neutrons = self.mass_number - self.atomic_number
+        # Get electron counts per shell
+        num_shells, electron_counts = self._get_electron_counts_per_shell()
 
-        # Return (protons, electrons, neutrons)
-        return (num_protons, num_electrons, num_neutrons)
+        # Create shells
+        for i in range(num_shells):
+            shell = Circle(
+                radius=1 + self.shell_offset * i,
+                stroke_width=self.shell_stroke_width,
+                stroke_color=DARK_GRAY,
+            )
+            shells.add(shell)
+
+        # Create electrons
+        for idx, electron_count in enumerate(electron_counts):
+            for electron_idx in range(electron_count):
+                electron = Dot(radius=0.05, color=WHITE, stroke_width=0)
+                electron.move_to(shells[idx].point_from_proportion(electron_idx / electron_count))
+                electrons.add(electron)
+
+        # Return
+        return electrons, shells
+
+    # HELPER METHODS ===================================================================================================
+
+    def _get_electrons(self) -> int:
+        """Returns the number of electrons in the atom."""
+        return self.atomic_number
+
+    def _get_protons(self) -> int:
+        """Returns the number of protons in the atom."""
+        return self.atomic_number
+
+    def _get_neutrons(self) -> int:
+        """Returns the number of neutrons in the atom."""
+        return self.mass_number - self.atomic_number
+
+    def _get_electrons_in_shell(self, n: int) -> int:
+        """
+        Returns the number of electrons in shell n.
+
+        Args:
+            n: The shell number.
+
+        Returns:
+            The number of electrons in the shell.
+
+        Raises:
+            ValueError: Raises an exception if n is less than 1.
+        """
+        # Validate n
+        if n < 1:
+            raise ValueError("Shell number must be greater than 0.")
+
+        # Return
+        return 2 * n * n
+
+    def _get_electron_counts_per_shell(self) -> Tuple[int, List[int]]:
+        """
+        Returns the number of electrons per shell.
+
+        Args:
+            None
+
+        Returns:
+            Tuple[int, List[int]]: A tuple containing the number of shells and a list of electron counts per shell.
+
+        Raises:
+            None
+        """
+        # Get total electron count
+        electrons: int = self._get_electrons()
+
+        # Get electron counts per shell
+        electron_counts: List[int] = list()
+
+        # Iterate through each shell
+        while electrons > 0:
+            if electrons > 0:
+                shell_electrons = min(electrons, self._get_electrons_in_shell(len(electron_counts) + 1))
+                electron_counts.append(shell_electrons)
+                electrons -= shell_electrons
+            else:
+                break
+
+        # Return
+        return (len(electron_counts), electron_counts)
 
     # REPRESENTATION ===================================================================================================
 
@@ -195,6 +284,20 @@ class BohrAtom(VGroup):
     def __hash__(self) -> int:
         """Return a hash of the atom."""
         return hash((self.atom_type, self.atomic_number, self.mass_number))
+
+    # PUBLIC METHODS ===================================================================================================
+
+    def get_name(self) -> Tex:
+        """Return the name of the atom."""
+        return Tex(self.atom_type.name)
+
+    def get_atomic_number(self) -> Tex:
+        """Return the atomic number of the atom."""
+        return Tex(self.atomic_number)
+
+    def get_mass_number(self) -> Tex:
+        """Return the mass number of the atom."""
+        return Tex(self.mass_number)
 
 
 # ======================================================================================================================
