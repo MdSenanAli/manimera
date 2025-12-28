04355507233f444fd9cbc0ba29f708086c16f111
MohammadSenanAli <mdsenan@gmail.com>
2025-12-28 20:46:22 +0530

    feat: Added BohrAtom rotation logic for electrons.

--- src/manimera/animations/__init__.py
+++ src/manimera/animations/__init__.py
@@ -9,10 +9,12 @@ from .advance_clock_time import AdvanceClockTime
 from .pendulum_oscillation import PendulumOscillation
 from .lamp_glow import LampGlow
 from .fade_reveal import FadeReveal
+from manimera.animations.atom_rotate import AtomRotate
 
 __all__ = [
     "AdvanceClockTime",
     "PendulumOscillation",
     "LampGlow",
     "FadeReveal",
+    "AtomRotate",
 ]
